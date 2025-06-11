1e7ad0b1532c4cb87c6ebb78823f4bde43c57a24
Jacob-Parrott <jacobparrott57@gmail.com>
2025-06-11 13:29:45 -0500

    Lesson from 6/11/25

--- domManipulation.js
+++ domManipulation.js
@@ -11,6 +11,7 @@ let image = document.querySelector('.image');
 
 
 // CODE ALONG
+
 // 1. An event listener is provided for the `contentButton`
 // 2. When clicked, it should change the content of:
 //   - The `name` header.
@@ -22,24 +23,24 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+myName.innerHTML = "My name is Jacob Parrott"
+myHobby.innerHTML = "I dont have a hobby"
+myLocation.innerHTML = "I go to Nextech"
 });
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
 
-
-
-
+image1Button.addEventListener("click", function() {
+image.src="https://picsum.photos/200";
+});
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
 
+image2Button.addEventListener("click", function() {
+image.src="https://picsum.photos/200";
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
